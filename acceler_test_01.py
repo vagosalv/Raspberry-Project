95620629c8f486302f71fd817ca9e462a49f7d46
vagosalv <vagosalvgd@gmail.com>
2022-06-28 09:14:27 +0300

    Add files via upload

---
+++ acceler_test_01.py
@@ -0,0 +1,30 @@
+
+import time
+from datetime import datetime
+import board
+import adafruit_adxl34x
+import csv
+
+# uses board.SCL and board.SDA
+i2c = board.I2C()
+
+# For ADXL343
+accelerometer = adafruit_adxl34x.ADXL343(i2c)
+accelerometer.data_rate = adafruit_adxl34x.DataRate.RATE_200_HZ
+print(accelerometer.data_rate)
+
+#Header
+header = ["Time", "X-axis", "Y-axis", "Z-axis"]
+
+def processing_loop(csvfile):
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow([header]) 
+    while True:
+        Data = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f ") + "%f %f %f" % accelerometer.acceleration
+        print(Data) 
+        csv_writer.writerow([Data])
+        csvfile.flush()
+
+
+with open("dedomena.csv", "w") as csvfile:
+    processing_loop(csvfile)
